6e14344174575399a117a6e0cdb57ac420d6e758
Jesse B. Crawford <jesse@jbcrawford.us>
2013-10-29 18:06:43 -0600

    First part of actual runner implemented

---
+++ avocet.py
@@ -0,0 +1,21 @@
+from heavensabove import HAUtil, Satellite
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+def main():
+    utils = HAUtil(34.0854, -106.8914, 1403, "MST")
+
+    message = "Interesting astronomical events in the next ten days:\n\n"
+
+    iss = Satellite("International Space Station", 25544)
+
+    message += str(iss)
+
+    email = MIMEText(message)
+    email["To"]       = "atl@jbcrawford.us"
+    email["Subject"]  = "Upcoming astronomical events"
+    p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE)
+    p.communicate(msg.as_string())
+
+if __name__ == "__main__":
+    main()
